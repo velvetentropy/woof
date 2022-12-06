ba844f290b4e359c9c332790227fafc4723c0f7a
velvetentropy <ashutoshnarang80@gmail.com>
2022-12-07 00:10:12 +0530

    add event listener

--- woof/ViewController.swift
+++ woof/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dogImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        dogImageView.image = #imageLiteral(resourceName: "3")
+        dogImageView.alpha = 0.5
+        
+        
     }
 
 
